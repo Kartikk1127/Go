fb696b229534a159e00561a3927ab269f69a48fa
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-06-21 20:25:52 +0530

    simple values

---
+++ simple_values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+
+	//integer
+	fmt.Println(1)
+
+	//string
+	fmt.Println("hello golang")
+
+	//boolean
+	fmt.Println(true)
+
+	//float
+	fmt.Println(10.5)
+
+}
