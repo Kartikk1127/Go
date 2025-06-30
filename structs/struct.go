9618259dd9a3090da7aeb69b4130a3b41273aacc
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-06-30 06:53:56 +0530

    struct embedding

--- structs/struct.go
+++ structs/struct.go
@@ -8,6 +8,18 @@ import (
 // use pointers to modify the struct else not needed
 // if you don't set a particular field, it will fallback to its default value
 // there is this convention to start the names in UPPERCASE
+type customer struct {
+	name  string
+	phone string
+}
+
+func newCustomer(name string, phone string) customer {
+	customer := customer{
+		name:  name,
+		phone: phone,
+	}
+	return customer
+}
 
 // order struct - this is how you create a struct
 type order struct {
@@ -15,15 +27,17 @@ type order struct {
 	amount    float32
 	status    string
 	createdAt time.Time // nano seconds precision
+	customer            // -- this is an example of struct embedding
 }
 
 // this is the convention to form a constructor in Go, you have to return a pointer of the struct
-func newOrder(id string, amount float32, status string) *order {
+func newOrder(id string, amount float32, status string, customer customer) *order {
 	// initial setup goes here.
 	order := order{
-		id:     id,
-		amount: amount,
-		status: status,
+		id:       id,
+		amount:   amount,
+		status:   status,
+		customer: customer,
 	}
 	return &order
 }
@@ -42,7 +56,8 @@ func (o order) getAmount() float32 {
 // structs are custom data structures
 func main() {
 
-	order := newOrder("1", 30.5, "confirmed")
+	customer := newCustomer("kartikey", "9794159484")
+	order := newOrder("1", 30.5, "confirmed", customer)
 
 	order.createdAt = time.Now()
 
@@ -60,4 +75,6 @@ func main() {
 	}
 
 	fmt.Println(language)
+	fmt.Println(customer)
+	fmt.Println(order.customer.name)
 }
