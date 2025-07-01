2b4166c0683d94636f8bcb2b92b36f4ee57e094d
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2025-07-01 06:49:40 +0530

    added a comment

--- waitgroupingoroutine/waitgroup.go
+++ waitgroupingoroutine/waitgroup.go
@@ -7,6 +7,7 @@ import (
 
 // wait groups are used to ensure that all running goroutines were finished or not? If yes, can exit the program
 
+// make sure to receive the pointer
 func task(id int, w *sync.WaitGroup) {
 	defer w.Done() // defer basically runs after this method's execution
 	fmt.Println("Doing some task", id)
